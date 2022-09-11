3f4c2b9f47290c66e3cb3bf4ceb2fd408073122c
DadaLV <vynohradova.l.a@gmail.com>
2022-09-11 13:01:46 +0300

    6 task complete

--- TASKS/06-data-types/finish.js
+++ TASKS/06-data-types/finish.js
@@ -12,3 +12,18 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+const myString = "Hi";
+const number = 16;
+const boolean = true;
+const myNull = null;
+const something = undefined;
+const myObject = { a: 1 };
+const myArray = [3, 6, 9];
+console.log(typeof (myObject));
+console.log(typeof (number));
+console.log(typeof (boolean));
+console.log(typeof (myNull));
+console.log(typeof (myString));
+console.log(typeof (myArray));
+console.log(typeof (something));
+
