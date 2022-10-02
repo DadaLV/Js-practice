5a792bd41f2568d46d9d735171e45efcc8b01d83
DadaLV <vynohradova.l.a@gmail.com>
2022-10-02 14:12:10 +0300

    task 32 done

--- TASKS/32-add-start-of-the-array/finish.js
+++ TASKS/32-add-start-of-the-array/finish.js
@@ -7,8 +7,8 @@
  * 2. Выведите длину результирующего массива в консоль
  */
 
-const myArray = [true, null]
+const myArray = [true, null];
 
-// Напишите код здесь
-
-console.log(myArray) // [100, "Hello", true, null]
+myArray.unshift("Hello");
+myArray.unshift(100);
+console.log(myArray); // [100, "Hello", true, null]
